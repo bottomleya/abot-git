eee73a906e4670660fbe79de71d4f75d193ff909
bottomleya <bottomleya@hotmail.com>
2020-07-14 13:45:29 +1000

    Update skills.js

--- js/skills.js
+++ js/skills.js
@@ -1,83 +1,87 @@
-skills = {
-	"Frontend Development": [
-		{
-			"skill": "HTML/CSS",
-			"proficiency": 65
-		},
-		{
-			"skill": "JavaScript/JQuery",
-			"proficiency": 70
-		},		
-		{
-			"skill": "Bootstrap/AngularJS",
-			"proficiency": 20
-		},		
-		{
-			"skill": "Git/GitExtensions",
-			"proficiency": 70
-		},		
-		{
-			"skill": "Responsive Design",
-			"proficiency": 75
-		},		
-		{
-			"skill": "Bootstrap/AngularJS",
-			"proficiency": 20
-		}	
-	],
-	"Backend Development": [
-		{
-			"skill": "Python Web Frameworks [Flask, Django]",
-			"proficiency": 80
-		},
-		{
-			"skill": "REST API Architectures",
-			"proficiency": 80
-		},	
-		{
-			"skill": "Databases [MySQL, Microsoft SQL Server]",
-			"proficiency": 80
-		},	
-		{
-			"skill": "Cloud Services",
-			"proficiency": 70
-		},	
-		{
-			"skill": "Web Servers [IIS, NGINX]",
-			"proficiency": 70
-		}
-	], 
-	"Engineering Skills": [
-		{
-			"skill": "Project Management",
-			"proficiency": 85
-		},
-		{
-			"skill": "Thermodynamics",
-			"proficiency": 80
-		},	
-		{
-			"skill": "Collaboration/Teamwork",
-			"proficiency": 95
-		},	
-		{
-			"skill": "Quality Assurance",
-			"proficiency": 90
-		},	
-		{
-			"skill": "Engineering Processes",
-			"proficiency": 95
-		},	
-		{
-			"skill": "Problem Solving",
-			"proficiency": 100
-		},	
-		{
-			"skill": "Communication",
-			"proficiency": 95
-		}
-	]
-};
+class skills{
+	constructor() {
+		this.skills = {
+			"Frontend Development": [
+				{
+					"skill": "HTML/CSS",
+					"proficiency": 65
+				},
+				{
+					"skill": "JavaScript/JQuery",
+					"proficiency": 70
+				},		
+				{
+					"skill": "Bootstrap/AngularJS",
+					"proficiency": 20
+				},		
+				{
+					"skill": "Git/GitExtensions",
+					"proficiency": 70
+				},		
+				{
+					"skill": "Responsive Design",
+					"proficiency": 75
+				},		
+				{
+					"skill": "Bootstrap/AngularJS",
+					"proficiency": 20
+				}	
+			],
+			"Backend Development": [
+				{
+					"skill": "Python Web Frameworks [Flask, Django]",
+					"proficiency": 80
+				},
+				{
+					"skill": "REST API Architectures",
+					"proficiency": 80
+				},	
+				{
+					"skill": "Databases [MySQL, Microsoft SQL Server]",
+					"proficiency": 80
+				},	
+				{
+					"skill": "Cloud Services",
+					"proficiency": 70
+				},	
+				{
+					"skill": "Web Servers [IIS, NGINX]",
+					"proficiency": 70
+				}
+			], 
+			"Engineering Skills": [
+				{
+					"skill": "Project Management",
+					"proficiency": 85
+				},
+				{
+					"skill": "Thermodynamics",
+					"proficiency": 80
+				},	
+				{
+					"skill": "Collaboration/Teamwork",
+					"proficiency": 95
+				},	
+				{
+					"skill": "Quality Assurance",
+					"proficiency": 90
+				},	
+				{
+					"skill": "Engineering Processes",
+					"proficiency": 95
+				},	
+				{
+					"skill": "Problem Solving",
+					"proficiency": 100
+				},	
+				{
+					"skill": "Communication",
+					"proficiency": 95
+				}
+			]
+		};
+	}
+}
 		
 		
 		
